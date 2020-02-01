53b4bf472726fee0bd6777b3bf6fdee38d32a5f1
suyesheng <1245035229@qq.com>
2020-02-01 16:55:33 +0800

    add flist

---
+++ utils/flist.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, help='path to the dataset')
+parser.add_argument('--output', type=str, help='path to the file list')
+args = parser.parse_args()
+
+ext = {'.JPG', '.JPEG', '.PNG', '.TIF', 'TIFF'}
+
+flist_dataset_paths = {
+    r"../data/coco/trainvalno5k.txt":   [r"E:\DeepLearningData\YOLOv3\cocos\images\train2014",],
+    r"../data/coco/5k.txt":             [r"E:\DeepLearningData\YOLOv3\cocos\images\val2014",],
+}
+
+for flist_path, dataset_paths in flist_dataset_paths.items():
+    print('creating', flist_path)
+    images = []
+
+    # auto create directory
+    os.makedirs(os.path.dirname(flist_path), exist_ok=True)
+
+    # find all image
+    for dataset_path in dataset_paths:
+        for root, dirs, files in os.walk(dataset_path):
+            print('loading : ' + root)
+            for file in files:
+                # print('image : ' + file)
+                if os.path.splitext(file)[1].upper() in ext:
+                    images.append(os.path.join(root, file))
+
+    images = sorted(images)
+    np.savetxt(flist_path, images, fmt='%s')
